dc79e2e2df5f341a69b28cd8ec41550fb10f94a8
Alex <foster-13@yandex.ru>
2022-04-07 08:18:12 +0300

    way from images

--- src/App.js
+++ src/App.js
@@ -2,44 +2,45 @@ import React, { useState } from "react";
 import axios from "axios";
 
 import Maps from "./components/Maps";
+import Cloud from "./assets/cloud.png";
+import Sunny from "./assets/sunny.png";
+import Snow from "./assets/snow.png";
+import Rain from "./assets/rain.png";
 
 function App() {
   const [data, setData] = useState({});
   const [location, setLocation] = useState("");
-  const [coord, setCoord] = useState({});
+  
 
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=cbd884be8dfaeb7549452e820c2f3476&units=metric`;
 
   const weather = {
-    'Sunny': './assets/sunny.png',
-    'Clouds': './assets/cloud.png',
-    'Rain': './assets/rain.png',
-    'Snow': './assets/snow.png',
+    'Clear': Sunny,
+    'Clouds': Cloud,
+    'Rain': Rain,
+    'Snow': Snow,
   }
-
-  
 
   const searchLocation = (event) => {
     if (event.key === "Enter") {
       axios.get(url).then((response) => {
-        setData(response.data);
-        
+        setData(response.data);   
         console.log(response.data);
       });
       setLocation("");
     }
   };
 
-  // const weatherNow = () => {
-    
-  //   Object.keys(weather).map((wear) => {
-  //     if(wear == data.weather[0].main){
-  //       return (<h1>hello</h1>)
-  //     }  
-  //   })
-  // }
+  const weatherNow = () => {  
+    for (let wear in weather) {
+      if (wear == data.weather[0].main) {
+        return (<img src={weather[wear]} alt="weather" className="img_weather" />)
+      }  
+    }
+  }
 
   return (
+    
     <div className="app">
       <div className="search">
         <input
@@ -60,7 +61,8 @@ function App() {
             {data.main ? <h1>{Math.floor(data.main.temp)}°C</h1> : null}
           </div>
 
-          {/* <div className="weather_img">{data.weather?weatherNow():null}</div> */}
+          <div className="weather_img">
+            {data.weather ? weatherNow() : null}</div>
           
           
           
